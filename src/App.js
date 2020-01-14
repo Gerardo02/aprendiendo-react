1f0668397929c528e9151895b8df8735191f3aaa
Moises Alberto Rodriguez Aceves <moises@MacBook-Pro-de-Moises.local>
2020-01-14 08:29:20 -0700

    update

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { HashRouter as Router, Route, Switch, Link } from "react-router-dom";
 import Principal from "./pages/principal";
 import captura from "./pages/captura";
 import inventario from "./pages/inventario";
+import inventarioCompleto from "./pages/inventarioCompleto";
 import historial from "./pages/historial";
 import bajas from "./pages/bajas";
 import add from "./pages/addSel";
@@ -21,6 +22,7 @@ class App extends Component {
             <Route exact path="/historial" component={historial} />
             <Route exact path="/bajas" component={bajas} />
             <Route exact path="/inventario1" component={inventario} />
+            <Route exact path="/inventarioCompleto" component={inventarioCompleto} />
             <Route exact path="/captura" component={captura} />
             <Route exact path="/add" component={add} />
             <Route exact path="/actualizar" component={actualizar} />
---
+++ src/pages/inventarioCompleto.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import "../inve.css";
+import Inve from "../components/inve";
+import Inve2 from "../components/inve2";
+import Filtros from "../components/filtros";
+import superlogo from "../images/super-logo.png";
+import { Link } from "react-router-dom";
+class inventarioCompleto extends Component {
+  render() {
+    return (
+      <>
+        <div className="barra-nav">
+          <Link className="textobarra" to="/">
+            <img className="superlogo" src={superlogo} />
+          </Link>
+          <Link className="textobarra" to="/captura">
+            Captura
+          </Link>
+          <Link className="textobarra" to="/inventario1">
+            Inventario
+          </Link>
+          <Link className="textobarra" to="/historial">
+            Historial
+          </Link>
+          <Link className="textobarra" to="/bajas">
+            Bajas
+          </Link>
+          <Link className="textobarra" to="/reportes">
+            Reportes
+          </Link>
+        </div>
+
+        <br />
+        <br />
+
+        <Inve2 />
+      </>
+    );
+  }
+}
+
+export default inventarioCompleto;
